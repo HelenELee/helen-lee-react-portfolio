b6f1fa48dfe1b347f0a3ffc408e4c208658e7528
HelenELee <HelenELee3@outlook.com>
2023-04-18 13:35:27 +0800

    added project data and use in Project component

--- src/project-data.js
+++ src/project-data.js
@@ -1,25 +1,27 @@
 export const projectData = [
     {"id": "1",
      "title": "Pumping Party Planner",
-     "app-link": "https://www.google.au",
+     "app": "https://www.myosh.com",
      "github": "https://github.com/caoimhejyoti/pumping-party-planner",
-     "img": "ppp_20.png"
+     "img": "pppImage",
+     "desc": "A web application which allows users to search for music or cocktails to save for their party plans."
     },
     {"id": "2",
      "title": "Cyber-fridge",
-     "app-link": "https://www.google.au",
+     "app": "https://www.google.au",
      "github": "https://github.com/HelenELee/cyber-fridge",
-     "img": "cyberfridge.png"
+     "img": "cyberfridge.png",
+     "desc": "An inventory management application to track the food items in a users fridge. It allows logged in users to add/update/delete quantities of food items in their fridge and create recipes from these food items."
     },
     {"id": "3",
      "title": "Work Day Scheduler",
-     "app-link": "https://www.google.au",
+     "app": "https://www.google.au",
      "github": "https://github.com/HelenELee/cyber-fridge",
      "img": "ppp_20.png"
     },
     {"id": "4",
      "title": "Note Taker",
-     "app-link": "https://www.google.au",
+     "app": "https://www.google.au",
      "github": "https://github.com/HelenELee/cyber-fridge",
      "img": "ppp_20.png"
     }
