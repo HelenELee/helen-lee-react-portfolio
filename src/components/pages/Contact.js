30c6a19cd6e842f77c748e320411a81d87f5d45d
HelenELee <HelenELee3@outlook.com>
2023-04-18 13:37:11 +0800

    added basic fields and validation

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,88 +1,93 @@
-
+import React, { useState } from 'react';
 
 function Form() {
-  const handleOnBlur = (e) => {
-    const { target } = e;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleInputChange = evt => {
+    //setName(evt.target.value);
+
+    const { target } = evt;
     const inputType = target.name;
-    //const inputValue = target.value;
+    const inputValue = target.value;
 
     if (inputType === 'email') {
-      //setEmail(inputValue);
-      alert('email');
+      setEmail(inputValue);
     } else if (inputType === 'userName') {
-      //setUserName(inputValue);
-      alert('username');
+      setName(inputValue);
     } else {
       //setMessage(inputValue);
       alert("message");
-    }
-    return;
   }
+}
 
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
-
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-   /* if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
-    */
-    
-   
-
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
-    //setUserName('');
-    //setMessage('');
-//setEmail('');
-  };
-
-
+  const isValidName = name !== "";
+  const isValidEmail = email !== "";
+  const [enteredName, setEnteredName] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState(false);
 
   return (
-    <div>
 
-      <form className="form">
-        <label htmlFor="userName">Name:</label>
+    <main className="w-3/4 pt-20 m-auto">
+      
+        <form className="form">
+        <label htmlFor="userName" className="block">Name:</label>
         <input
           id="userName"
           name="userName"
-          //onChange={handleInputChange}
+          value={name}
+          onChange={handleInputChange}
           type="text"
          // placeholder="username"
-         //onBlur={handleOnBlur}
+         onBlur={() => setEnteredName(true)}
+         className="appearance-none rounded-none relative
+         w-3/4 px-3 py-2 border border-gray-300
+         placeholder-gray-500 text-gray-900 rounded
+         focus:outline-none focus:ring-indigo-500
+         focus:border-indigo-500 focus:z-10 sm:text-sm"
         />
-        <label htmlFor="email">Email:</label>
+        {enteredName ? (isValidName ? "✅" : "❌") : null}
+        <label htmlFor="email" className="block">Email:</label>
         <input
           id="email"
           name="email"
-         // onChange={handleInputChange}
+          onChange={handleInputChange}
           type="email"
+          value={email}
          // placeholder="email"
-          //onBlur={handleOnBlur}
+          onBlur={() => setEnteredEmail(true)}
+          className="appearance-none rounded-none relative
+          w-3/4 px-3 py-2 border border-gray-300
+          placeholder-gray-500 text-gray-900 rounded
+          focus:outline-none focus:ring-indigo-500
+          focus:border-indigo-500 focus:z-10 sm:text-sm"
         />
-        <label htmlFor="message">Message:</label>
+        {enteredEmail ? (isValidEmail ? "✅" : "❌") : null}
+        <label htmlFor="message" className="block">Message:</label>
         <input
           //value={password}
           name="message"
          // onChange={handleInputChange}
           type="text"
-         // placeholder="Enter your message"
+          placeholder="Enter your message"
          //onBlur={handleOnBlur}
+         className="appearance-none rounded-none relative
+         w-3/4 px-3 py-2 border border-gray-300
+         placeholder-gray-500 text-gray-900 rounded
+         focus:outline-none focus:ring-yellow-500
+         focus:border-yellow-500 focus:z-10 sm:text-sm h-20"
         />
-        <button type="button" >Submit</button>
+        <button type="button" className="group relative flex justify-center
+                my-5 py-2 px-4 border border-transparent text-sm font-medium
+                rounded-md text-white bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2
+                focus:ring-indigo-500">Submit</button>
       </form>
-      {/*
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
-      */}
-    </div>
+      
+     
+      
+      
+    </main>
+    
   );
 }
 
