1f6b2651840b3fe988bb2d773f09e2a252dad34d
HelenELee <HelenELee3@outlook.com>
2023-04-18 13:36:26 +0800

    added props

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,11 +5,11 @@ import  {projectData} from '../../project-data';
 export default function Portfolio() {
   return (
     
-    <main className="w-3/4 pt-20 m-auto flex flex-row border-2 justify-center flex-wrap">
+    <main className="w-3/4 pt-20 m-auto flex flex-row justify-center flex-wrap">
       {
         projectData.map((proj) => {
           console.log(proj.title);
-          return <Project id={proj.id} title={proj.title}/>
+          return <Project id={proj.id} title={proj.title} img={proj.img} app={proj.app} git={proj.github} desc={proj.desc}/>
         })
       }
       
