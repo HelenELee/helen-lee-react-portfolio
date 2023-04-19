9aa3a1302802dc11616f7b23f2b0ef00e558e722
HelenELee <HelenELee3@outlook.com>
2023-04-19 22:30:01 +0800

    changes to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import profileImage from "../../assets/HelenLee.jpg"
+//import profileImage from "../../assets/HelenLee.jpg";
+import helenImage from "../../assets/helen-framed.png";
 
 export default function About() {
   return (
     <main className="w-3/4 pt-20 m-auto flex flex-row">
       <aside className="m-auto p-10">
-        <img src={profileImage} alt="Helen Lee head shot" class="photo" />
+        <img src={helenImage} alt="Helen Lee head shot" class="photo" />
       </aside>
       <section>
           <h1>Hi, I'm Helen Lee</h1>
-
-          <p> I am a full stack developer based in Perth Australia and have completed the UWA Full Stack Developer Bootcamp course. 
+          <p> I have been a Lotus Notes developer in am a full stack developer based in Perth Australia and have completed the UWA Full Stack Developer Bootcamp course. 
             Please check out my work in the Portfolio section.</p>
 
           <p>I have worked in the IT industry in different roles including developer, business analyst, project leader and 
@@ -19,6 +19,8 @@ export default function About() {
           <p>I thrive on producing great applications that exceed the clients requirements!</p>
 
           <p>When not coding I love to excercise, read and hang out with family and friends.     </p>
+     
+
       </section>
       
     </main>
