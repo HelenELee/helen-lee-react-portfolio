155383d31549d3deb334ba519e4473f788742e6b
HelenELee <HelenELee3@outlook.com>
2023-04-19 22:40:14 +0800

    moved data and updated css

---
+++ src/components/data/project-data.js
@@ -0,0 +1,32 @@
+import pppImage from '../../assets/ppp_20.png';
+import cyberFImage from '../../assets/cyberfridge.png';
+import workdaySchedImage from '../../assets/work-day-schedular.png';
+
+export const projectData = [
+    {id: "1",
+     title: "Pumping Party Planner",
+     app: "https://www.myosh.com",
+     github: "https://github.com/caoimhejyoti/pumping-party-planner",
+     img: pppImage,
+     desc: "A web application which allows users to search for music or cocktails to save for their party plans."
+    },
+    {id: "2",
+     title: "Cyber-fridge",
+     app: "https://www.google.au",
+     github: "https://github.com/HelenELee/cyber-fridge",
+     img: cyberFImage,
+     desc: "An inventory management application to track the food items in a users fridge. It allows logged in users to add/update/delete quantities of food items in their fridge and create recipes from these food items."
+    },
+    {id: "3",
+     title: "Work Day Scheduler",
+     app: "https://www.google.au",
+     github: "https://github.com/HelenELee/cyber-fridge",
+     img: workdaySchedImage
+    },
+    {id: "4",
+     title: "Note Taker",
+     app: "https://www.google.au",
+     github: "https://github.com/HelenELee/cyber-fridge",
+     img: "ppp_20.png"
+    }
+]
---
+++ src/components/data/resume-data.js
@@ -0,0 +1,7 @@
+export const resumeDataFE = [
+"HTML", "CSS", "Javascript", "JQuery", "Responsive Design", "React", "Bootstrap", "Tailwind"
+]
+
+export const resumeDataBE = [
+    "APIs", "Node.js", "Express", "MySQL", "Sequelize", "MongoDB", "Mongoose"
+]
