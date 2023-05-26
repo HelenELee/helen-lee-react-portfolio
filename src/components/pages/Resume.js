95966f145447ec56c1ac26baae4cdf37f0817a48
HelenELee <HelenELee3@outlook.com>
2023-05-26 10:46:23 +0800

    added link to updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,7 +5,9 @@ export default function Resume() {
   return (
     <main className="w-3/4 pt-20 m-auto flex flex-col justify-center">
       <div className="flex flex-row justify-center">
-        <a href="https://drive.google.com/file/d/1VMWFiiijRbdrj_syCVhSilMFpDtUWZ_m/view?usp=share_link" className="inline-flex items-center text-blue-600 hover:underline mt-5 text-center">Link to Full Resume <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" target="_blank" rel="noopener noreferrer"></path></svg></a>
+        {/* <a href="https://drive.google.com/file/d/1VMWFiiijRbdrj_syCVhSilMFpDtUWZ_m/view?usp=share_link" className="inline-flex items-center text-blue-600 hover:underline mt-5 text-center">Link to Full Resume <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" target="_blank" rel="noopener noreferrer"></path></svg></a> */}
+        <a href="https://drive.google.com/file/d/1_UNrZPbut9Cb85W5WiQXGk-rpZep68Fl/view?usp=drive_link" className="inline-flex items-center text-blue-600 hover:underline mt-5 text-center">Link to Full Resume <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" target="_blank" rel="noopener noreferrer"></path></svg></a>
+        
       </div>
       
       <div className="flex flex-row justify-center">
