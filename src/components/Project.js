77bd01cf234396d31b4d89a1991996026792ad1a
HelenELee <HelenELee3@outlook.com>
2023-05-26 14:56:14 +0800

    fixed issue with box shadow around projects on hover

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,8 +7,8 @@ export default function Project(props) {
    
 
     return (
-        <section className="max-w-sm bg-white border border-yellow-200 rounded-lg shadow  m-5 hover:shadow-[0_35px_60px_-15px_rgba(51,53,51,1.0)] hover:shadow-inner">            
-            
+        // <section className="max-w-sm bg-white border border-yellow-200 rounded-lg shadow  m-5 hover:shadow-[0_35px_60px_-15px_rgba(51,53,51,1.0)] hover:shadow-inner">            
+        <section className="max-w-sm bg-white border border-yellow-200 rounded-lg m-5 hover:shadow-orange hover:shadow-inner">             
             <a href="{props.app}">
                 <img src={props.img} alt={props.title + " home page image"} className="rounded-t-lg m-auto mt-5" />           
             </a>
@@ -19,7 +19,7 @@ export default function Project(props) {
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.desc}</p>
                 <a href={props.app} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-stone-500 rounded-lg hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-yellow-300 ">
                     Open Application
-                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </a>
                 
                 <a href={props.github} className="inline-flex items-center text-blue-600 hover:underline mt-5 text-center">
