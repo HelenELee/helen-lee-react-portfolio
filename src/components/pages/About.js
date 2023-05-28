ed4ba5fde6d518f134c927174e0162a1c38c447f
HelenELee <HelenELee3@outlook.com>
2023-05-28 20:49:32 +0800

    added last line of text and updates to photo

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,15 +6,15 @@ import helenImage from "../../assets/helen-beach-black-white-curl.jpg";
 
 export default function About() {
   return (
-    <main className="m-auto flex flex-row">
-      {/* <aside className="m-auto pr-10 w-2/4 h-2/4"> */}
-        {/* <img src={helenImage} alt="Helen Lee head shot" className="photo" /> */}
+    <main className="m-auto flex flex-row main-container">
         <aside className="pr-10 bg-dark hover:bg-light">
-  
+          {/* space for photo */}
         </aside>
-
+      {/* <aside className="image-container"> */}
+        {/* aside aside aside */}
+        {/* <img src={helenImage} alt="helen head shot"></img> */}
       {/* </aside> */}
-      <section className="w-2/4 pt-20 pl-20">
+      <section className="w-2/4 pt-20 pl-20 about-container">
           <h1 className="about-me mb-5">Hi, I'm Helen Lee.</h1>
           <p className="about-me mb-5"> Welcome to my portfolio! </p>
           <p className="about-me mb-5"> I am a developer based in beautiful Perth, in Western Australia. 
@@ -23,12 +23,14 @@ export default function About() {
           I am passionate about creating user friendly applications that simplify peoples lives. </p>
           <p className="about-me mb-5">Having completed a Full Stack Developer bootcamp course at UWA 
           I am now moving into the next phase of my career….and excited about it!! I have added React, 
-          Javascript, MongoDb, MySQL, Node, CSS, HTML and much more to my skillset and am looking for 
+          Javascript, MongoDb, MySQL, Node, GraphQL, CSS, HTML and much more to my skillset and am looking for 
           an opportunity where I can continue to grow and learn whilst becoming a valuable company asset! </p>
           
           <p className="about-me mb-10"> When not coding I love to excercise, read and hang out with family and friends.     </p>
-          {/* <p className="about-me">No matter what your current ability is, effort is what ignites that ability and
-turns it into accomplishment – Carol Dweck, Mindset</p> */}
+          
+          <p className="mb-10 highlight hand-written text-3xl text-center hover:underline pb-5"><a href="mailto:helenelee3@outlook.com?subject=Opportunity">Would love to hear from you <br></br>if you have an opportunity for me!!.</a></p>
+          
+
       </section>
       
     </main>
