25189598a48099fdc709e47c098dc8019a237d4e
HelenELee <HelenELee3@outlook.com>
2023-05-28 20:47:15 +0800

    added third column to resume and changes in colours

--- src/components/data/resume-data.js
+++ src/components/data/resume-data.js
@@ -1,7 +1,11 @@
 export const resumeDataFE = [
-"HTML", "CSS", "Javascript", "JQuery", "Responsive Design", "React", "Bootstrap", "Tailwind"
+"HTML", "CSS", "Javascript", "JQuery", "Responsive Design", "HandleBars", "React", "Bootstrap", "Tailwind", "Styled Components", "GraphQL/Apollo Client"
 ]
 
 export const resumeDataBE = [
-    "APIs", "Node.js", "Express", "MySQL", "Sequelize", "MongoDB", "Mongoose"
+    "APIs", "Node.js", "Express", "MySQL", "Sequelize", "MongoDB", "Mongoose", "GraphQL/Apollo Server", "Axios"
+]
+
+export const resumeDataOther = [
+    "JSON Web Token", "BCrypt", "ReCharts", "Font Awesome", "Full Calendar", "GitHub", "Heroku", "Atlas"
 ]
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {resumeDataFE, resumeDataBE } from '../data/resume-data'
+import {resumeDataFE, resumeDataBE, resumeDataOther } from '../data/resume-data'
 //link to full resume and skills
 export default function Resume() {
   return (
     <main className="w-3/4 pt-20 m-auto flex flex-col justify-center">
-      <div className="flex flex-row justify-center">
+      <div className="flex flex-col justify-center">
+        <span className="text-center about-me text-xl highlight text-4xl">Below is a snapshot of my skills. <br></br>Please check out my <a href="https://github.com/HelenELee" className="hover:curser-grabbing hover:underline">GitHub</a> account to see how I have used them.</span>
         {/* <a href="https://drive.google.com/file/d/1VMWFiiijRbdrj_syCVhSilMFpDtUWZ_m/view?usp=share_link" className="inline-flex items-center text-blue-600 hover:underline mt-5 text-center">Link to Full Resume <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" target="_blank" rel="noopener noreferrer"></path></svg></a> */}
-        <a href="https://drive.google.com/file/d/1_UNrZPbut9Cb85W5WiQXGk-rpZep68Fl/view?usp=drive_link" className="inline-flex items-center text-blue-600 hover:underline mt-5 text-center">Link to Full Resume <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" target="_blank" rel="noopener noreferrer"></path></svg></a>
-        
+        {/* <a href="https://drive.google.com/file/d/1_UNrZPbut9Cb85W5WiQXGk-rpZep68Fl/view?usp=drive_link" className="inline-flex items-center text-red-600 hover:underline mt-5 text-center">Link to Full Resume <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" target="_blank" rel="noopener noreferrer"></path></svg></a> */}
+        <span className="inline-flex items-center highlight m-auto"><a href="https://drive.google.com/file/d/1_UNrZPbut9Cb85W5WiQXGk-rpZep68Fl/view?usp=drive_link" className="inline-flex items-center hover:underline mt-5 text-center">Link to Full Resume <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" target="_blank" rel="noopener noreferrer"></path></svg></a></span>
       </div>
       
       <div className="flex flex-row justify-center">
@@ -33,6 +34,16 @@ export default function Resume() {
               </ul>
           </section>
 
+          <section className="p-10">
+              <p className="block text-orange-600">And more....</p>
+              <ul className="list-disc">
+                {resumeDataOther.map((item, index) => (
+                  <li className="list-group-item" key={index}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+          </section>
       </div>
       
     </main>
